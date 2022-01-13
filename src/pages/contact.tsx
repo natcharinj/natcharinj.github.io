14506baf3d9a0a6515250d3c554270a3834ca4dd
Natcharin Janhom <natcharinj@petpaw.com>
2022-01-13 16:09:39 +0700

    add react-helmet package

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,13 +9,9 @@ export default function ContactPage() {
     <PageLayout>
       <div className='container'>
         <Helmet title={`Contact | ${config.siteTitle}`} />
-        <h2>Contact</h2>
+        <h2>My Contact</h2>
         <p>
           <h2>Edit your contact here:</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Sed sagittis pharetra ipsum, non varius massa tincidunt sit amet.
-          Cras id libero et urna sodales commodo. Donec nec aliquam turpis. Nulla et vestibulum erat. Fusce posuere risus id pellentesque sollicitudin. Sed quis nisi in lacus feugiat commodo. Praesent magna mauris, ornare at quam nec, iaculis hendrerit dolor. Maecenas quis metus malesuada, feugiat quam a, dignissim elit. Sed eget odio orci. Vestibulum eu ex vitae purus aliquam commodo.
-          Nunc suscipit metus augue, non tincidunt mauris aliquet vel.
         </p>
       </div>
     </PageLayout>
